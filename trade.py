fd1b494642e83a9db88e103de4b0e7805799c694
Jared Byers <jared_byers@icloud.com>
2018-01-05 10:18:24 -0800

    Updated print formatting on trade.py

--- trade.py
+++ trade.py
@@ -4,11 +4,12 @@ from decimal import *
 
 # Buy coins
 def buy(capital, price):
-    print("%s of coins bought for %s" % (str(Decimal(capital) / Decimal(price)), str(price)))
+    print("\n%s of coins bought for %s\n" % (str(Decimal(capital) / Decimal(price)), str(price)))
     return Decimal(capital) / Decimal(price)
 
 
 # Sell coins
 def sell(price, amount):
-    print("%s of coins sold for %s\nProfit: %s BTC " % (str(amount), str(price), str(Decimal(price) * Decimal(amount))))
+    print("\n%s of coins sold for %s\nProfit: %s BTC\n" % (str(amount), str(price),
+                                                           str(Decimal(price) * Decimal(amount))))
     return Decimal(price) * Decimal(amount)
