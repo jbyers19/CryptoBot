660e27091b983ca24c133959c2586f10e7065626
Jared Byers <jared_byers@icloud.com>
2018-10-21 11:20:13 -0700

    cleanup before pushing to remote

--- algorithm.py
+++ algorithm.py
@@ -8,28 +8,35 @@ def price_queue_init(array_length):
     array = []
     for i in range(array_length):
         array.append(get_price())
-        time.sleep(3)
+        time.sleep(1)
     print("\nInitial Price Queue:", str(array), "\n")
     return array
 
 
 # Updates the price queue with the latest price
 def price_queue(array):
-    i = 0
-    array_length = len(array) - 1
-    while i < array_length:
-        array[i] = array[i + 1]
-        i += 1
-    array[array_length] = get_price()
-    print("Updated Queue: ", str(array))
+    current_price = get_price()
+
+    if array[-1] != current_price:
+        i = 0
+        array_length = len(array) - 1
+        while i < array_length:
+            array[i] = array[i + 1]
+            i += 1
+        array[array_length] = current_price
+        # print("Updated Queue: ", str(array))
+
     return array
 
 
 # Determines the price trend and whether to buy, sell, or hold
 def get_trend(array):
-    if array[0] >= array[1] < array[2] < array[3]:
+
+    if array[0] >= array[1] >= array[2] < array[3] < array[4] < array[5]:
         return "Buy"
-    elif array[0] <= array[1] > array[2] > array[3]:
+
+    elif array[0] <= array[1] <= array[2] > array[3] > array[4] > array[5]:
         return "Sell"
+
     else:
         return "Hold"
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import sys
 
 
 # Send all print()s to stdout
-sys.stdout = open("/Users/Jared/Desktop/CryptoBot.txt", "w")
+sys.stdout = open("./CryptoBot.txt", "w")
 
 
 # Timestamp
@@ -30,33 +30,40 @@ price = 0.00
 # Main method
 def main():
     global capital, num_coins, price
-    trend = price_queue_init(4)
+    trend = price_queue_init(6)
 
     while True:
-        timestamp()
         trend = price_queue(trend)
         trade_status = get_trend(trend)
         price = trend[-1]
 
         if trade_status == "Buy" and capital > 0:
+            timestamp()
             price = get_price()
             num_coins = buy(capital, price)
             capital = 0.00
             print("\n-- Bought coins! --\n")
+            print("Capital: %s BTC" % str(capital))
+            print("Number of Coins:", num_coins)
+            print("Current price: %s BTC" % str(price))
+            print("\n=================================================\n")
+
         elif trade_status == "Sell" and num_coins > 0:
+            timestamp()
             price = get_price()
             capital = sell(price, num_coins)
             num_coins = 0
             print("\n-- Sold coins! --\n")
+            print("Capital: %s BTC" % str(capital))
+            print("Number of Coins:", num_coins)
+            print("Current price: %s BTC" % str(price))
+            print("\n=================================================\n")
+
         else:
             pass
 
-        print("Capital: %s BTC" % str(capital))
-        print("Number of Coins:", num_coins)
-        print("Current price: %s BTC" % str(price))
-        print("\n=================================================\n")
 
-        time.sleep(30)
+        time.sleep(5)
 
 
 # Start script if main
